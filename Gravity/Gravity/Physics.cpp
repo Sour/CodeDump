ffae8accc18eaf883540852127f8c658c9e07bc4
Wil Gunter <in.wil.i.trust@gmailcom>
2013-05-24 01:52:07 -0700

    1.0.0

---
+++ Gravity/Gravity/Physics.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "Physics.h"
+
+float Physics::Distance(sf::Vector2f a, sf::Vector2f b )
+{
+	return sqrt( ( ( a.x - b.x ) * ( a.x - b.x ) ) + ( ( a.y - b.y ) * ( a.y - b.y ) ) );
+}
+
+bool Physics::RectvsRect( Rect a, Rect b )
+{
+	if ( a.max.x < b.min.x || a.min.x > b.max.x ) return false;
+	if ( a.max.y < b.min.y || a.min.y > b.max.y ) return false;
+	return true;
+}
+
+bool Physics::CirclevsCircle( Circle a, Circle b )
+{
+	return ( a.radius + b.radius ) < Distance( a.position, b.position );
+}
+
---
+++ Gravity/Gravity/Physics.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "stdafx.h"
+
+class Physics 
+{
+private:
+
+public:
+	
+	struct Rect 
+	{
+		sf::Vector2f min;
+		sf::Vector2f max;
+	};
+
+	struct Circle
+	{
+		sf::Vector2f position;
+		float radius;
+	};
+
+	bool RectvsRect( Rect a, Rect b );
+	bool CirclevsCircle( Circle a, Circle b );
+
+	void ResolveCollision( Object a, Object b );
+
+	float Distance( sf::Vector2f a, sf::Vector2f b );
+};
